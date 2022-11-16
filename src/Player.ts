2e387fe2396372e9d04a9e4351c7b5de28db1923
Kaarel Kann <kann@rushstreetinteractive.com>
2022-11-16 11:04:51 +0200

    min raise fix

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,6 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(gameState.minimum_raise);
+    betCallback(gameState.current_buy_in - gameState.players.in_action.bet + gameState.minimum_raise);
   }
 
   public showdown(gameState: any): void {
