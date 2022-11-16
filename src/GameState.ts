5e8384f2154d62c2ba9df3d9219e42305e1241a6
Leelo Rand <rand@rushstreetinteractive.com>
2022-11-16 11:40:30 +0200

    gamestate interface fix

--- src/GameState.ts
+++ src/GameState.ts
@@ -1,26 +1,27 @@
-
-export class GameState{
-    tournamentId: string;
-    gameId: string;
+export class GameState {
+    tournament_id: string;
+    game_id: string;
     round: number;
-    betIndex: number;
-    smallBlind: number;
+    bet_index: number;
+    small_blind: number;
     current_buy_in:number;
     pot: number;
     minimum_raise: number;
     dealer: number;
     orbits: number;
-    inAction: number;
-    players: {
-        id: number;
-        name: string;
-        status: string;
-        version: string;
-        stack: number;
-        bet: number;
-    }
-    communityCards: {
-        rank: string;
-        suit: string;
-    }
+    in_action: number;
+    players: Player[];
+    community_cards: CommunityCard[]
+}
+interface Player {
+    id: number;
+    name: string;
+    status: string;
+    version: string;
+    stack: number;
+    bet: number;
+}
+interface CommunityCard {
+    rank: string;
+    suit: string;
 }
